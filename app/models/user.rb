408f6c266c9db139a4b4bad9e5537d93c60c636e
Jan Czizikow <jan.czizikow@gmail.com>
2018-09-04 02:09:42 +0200

    Add magic comment everywhere

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
+  # :registerable, :recoverable, :rememberable, :validatable
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, #:registerable,
-         # :recoverable, :rememberable, :validatable
-         :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
+  devise :database_authenticatable,
+         :jwt_authenticatable,
+         jwt_revocation_strategy: JwtBlacklist
 end
