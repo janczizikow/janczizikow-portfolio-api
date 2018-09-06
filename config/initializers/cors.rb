b2e8ed3bbcfbff153e40da38c2a1828b43cd994d
Jan Czizikow <jan.czizikow@gmail.com>
2018-09-06 16:56:19 +0200

    CORS: remove localhost

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', # FIXME: keeping localhost for testing for now
-             'https://www.janczizikow.com'
+    origins 'https://www.janczizikow.com'
 
     resource '/api/v1/*',
       methods: %i[get post put patch delete options],
