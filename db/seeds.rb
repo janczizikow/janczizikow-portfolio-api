4893c94360c8ff222caa7473c1ba3b81465f6b6e
Jan Czizikow <jan.czizikow@gmail.com>
2018-09-11 02:16:53 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#frozen_string_literal: true
+
+require 'yaml'
+
+puts 'Generating seeds...'
+
+file = Rails.root.join('db', 'projects.yml')
+projects = YAML.safe_load(File.read(file))
+projects.each { |project| Project.create! project }
+
+puts 'All done!'
