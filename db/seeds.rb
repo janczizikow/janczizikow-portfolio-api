ce4973bfc6bacc146d0b9e0d0cf9d469460bdeb0
Jan Czizikow <jan.czizikow@gmail.com>
2018-09-11 20:38:31 +0200

    Fix: seeds projects order

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@ puts 'Generating seeds...'
 
 file = Rails.root.join('db', 'projects.yml')
 projects = YAML.safe_load(File.read(file))
-projects.each { |project| Project.create! project }
+projects.reverse.each { |project| Project.create! project }
 
 puts 'All done!'
