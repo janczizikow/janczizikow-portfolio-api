7fcf0a61cc2cddcf6894330ef0b671b833d75560
Jan Czizikow <jan.czizikow@gmail.com>
2019-12-18 03:35:55 +0100

    chore: run seeds only if not present in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,26 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
 require 'yaml'
 
 puts 'Generating seeds...'
 
-# puts 'Generating projects...'
+if Project.count < 1
+  puts 'Generating projects...'
+  projects_file = Rails.root.join('db', 'projects.yml')
+  projects = YAML.safe_load(File.read(projects_file))
+  projects.reverse.each { |project| Project.create! project }
+  puts 'Projects generated!'
 
-# projects_file = Rails.root.join('db', 'projects.yml')
-# projects = YAML.safe_load(File.read(projects_file))
-# projects.reverse.each { |project| Project.create! project }
+  puts 'Generating projects photos...'
 
-# puts 'Projects generated!'
+  photos_file = Rails.root.join('db', 'photos.yml')
+  photos = YAML.safe_load(File.read(photos_file))
+  photos.each do |photo|
+    project = Project.find_by(name: photo['name'])
+    photo['photos']&.each { |url| Photo.create!(project: project, remote_photo_url: url)}
+  end
 
-puts 'Generating projects photos...'
-
-photos_file = Rails.root.join('db', 'photos.yml')
-photos = YAML.safe_load(File.read(photos_file))
-photos.each do |photo|
-  project = Project.find_by(name: photo['name'])
-  photo['photos'].each { |url| Photo.create!(project: project, remote_photo_url: url)}
+  puts 'Photos generated!'
 end
-
-puts 'Photos generated!'
 
 puts 'All done!'
